1474239bd6b742adcda5122f02e0cfa2f4697619
jmaciel12 <jmacielrampone@udesa.edu.ar>
2024-11-23 13:04:20 -0300

    Que lo buscado en el buscador por el usuario tenga al menos 3 caracteres, sino avisarle

--- js/resultadobusqueda.js
+++ js/resultadobusqueda.js
@@ -3,36 +3,40 @@ let querString = location.search;
 let querStringObj = new URLSearchParams(querString);
 let busqueda = querStringObj.get("search");
 
-// Construir la URL de búsqueda con el valor del campo de búsqueda
-let url = `https://dummyjson.com/recipes/search?q=${busqueda}`;
+// Verificar si el término  buscado tiene al menos 3 caracteres
+if (busqueda.length < 3) {
+  alert("El término de búsqueda debe tener al menos 3 caracteres.");
+} else {
+  // Construir la URL de búsqueda con el valor del campo de búsqueda
+  let url = `https://dummyjson.com/recipes/search?q=${busqueda}`;
 
-// Realizar la petición GET a la API
-fetch(url)
-  .then(function(res) {
-    return res.json();
-  })
-  .then(function(data) {
-    // Procesar los datos devueltos por la API
-    console.log(data.recipes);
+  // Realizar la petición GET a la API
+  fetch(url)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      // Procesar los datos devueltos por la API
+      console.log(data.recipes);
 
-    // Acceder al array "recipes" dentro de los datos devueltos
-    let arrayDeRecetas = data.recipes;
+      // Acceder al array "recipes" dentro de los datos devueltos
+      let arrayDeRecetas = data.recipes;
 
-    // Capturar el elemento HTML donde se desean agregar las recetas
-    let seccion = document.querySelector(".container-products");
+      // Capturar el elemento HTML donde se desean agregar las recetas
+      let seccion = document.querySelector(".container-products");
 
-    // Verificar si hay resultados
-    if (arrayDeRecetas.length === 0) {
-      // Mostrar mensaje de no resultados
-      seccion.innerHTML = "No se encontraron resultados.";
-    } else {
-      // Inicializar la variable para almacenar el HTML de las recetas
-      let allRecipesHTML = "";
+      // Verificar si hay resultados
+      if (arrayDeRecetas.length === 0) {
+        // Mostrar mensaje de no resultados
+        seccion.innerHTML = "No se encontraron resultados.";
+      } else {
+        // Inicializar la variable para almacenar el HTML de las recetas
+        let allRecipesHTML = "";
 
-      // Recorrer el array de recetas y organizar el HTML
-      for (let i = 0; i < arrayDeRecetas.length; i++) {
-        // Agregar el HTML de cada receta a la variable allRecipesHTML
-        allRecipesHTML += `
+        // Recorrer el array de recetas y organizar el HTML
+        for (let i = 0; i < arrayDeRecetas.length; i++) {
+          // Agregar el HTML de cada receta a la variable allRecipesHTML
+          allRecipesHTML += `
           <article class="product-card">
             <div class="product-details">
               <img src="${arrayDeRecetas[i].image}" alt="${arrayDeRecetas[i].name}">
@@ -44,13 +48,14 @@ fetch(url)
             </div>
           </article>
         `;
-      }
+        }
 
-      // Añadir las nuevas recetas al DOM sin borrar las existentes
-      seccion.innerHTML = allRecipesHTML;
-    }
-  })
-  .catch(function(e) {
-    // Mostrar mensaje de error
-    console.log("Error:", e);
-  });
+        // Añadir las nuevas recetas al DOM sin borrar las existentes
+        seccion.innerHTML = allRecipesHTML;
+      }
+    })
+    .catch(function (e) {
+      // Mostrar mensaje de error
+      console.log("Error:", e);
+    });
+}
